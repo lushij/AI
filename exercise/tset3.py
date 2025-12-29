92217089803dd6d347a58587147cfceb51ca3c23
lushij <766993892@qq.com>
2025-12-29 11:33:52 +0800

    matplotlib

---
+++ exercise/tset3.py
@@ -0,0 +1,13 @@
+"""
+    Created by PyCharm
+    User:lushiji
+    Date:2025/12/29
+    Time:上午10:42
+    To change this template use File | Settings | File Templates
+"""
+
+
+
+
+if __name__ == '__main__':
+    pass
